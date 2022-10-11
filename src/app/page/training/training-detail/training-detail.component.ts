e83124a9e6d90ff02ead82d1cb840f9d4b7548c7
Mehmet Akif Kaya <82850894+akifkaya0@users.noreply.github.com>
2022-10-11 17:29:44 +0300

    training detail page updated

--- src/app/page/training/training-detail/training-detail.component.ts
+++ src/app/page/training/training-detail/training-detail.component.ts
@@ -1,54 +1,112 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Globals } from 'src/app/entity/globals';
 import { MuscleGroup } from 'src/app/entity/muscle-group';
-
+import { MuscleGroupExercise } from 'src/app/entity/muscle-group-exercise';
+import { MuscleGroupDetailService } from 'src/app/service/muscle-group-detail.service';
+import { WorkoutDayService } from 'src/app/service/workout-day.service';
+import * as uuid from 'uuid';
 @Component({
   selector: 'app-training-detail',
   templateUrl: './training-detail.component.html',
   styleUrls: ['./training-detail.component.scss']
 })
-export class TrainingDetailComponent implements OnInit {
+export class TrainingDetailComponent implements OnInit , AfterViewChecked {
 
   global: Globals
   selectedMuscleGroups: MuscleGroup[] = [];
+  selectedMuscleGroupsView : MuscleGroup[] = [];
 
-  constructor(private activatedRoute: ActivatedRoute, global: Globals) { this.global = global }
+  constructor(private workoutDayService : WorkoutDayService, private detailService : MuscleGroupDetailService ,private activatedRoute: ActivatedRoute, global: Globals) { this.global = global }
+
+  ngAfterViewChecked(): void {
+
+  }
 
   ngOnInit(): void {
 
     this.activatedRoute.queryParams.subscribe(res => {
 
-      Object.values(res).map((item, index) => {
-        if (item == "true")
-          this.selectedMuscleGroups.push(this.global.muscleGroupDefaultEntities[this.global.allMuscleGroups[index + 1].name]);
-      })
+      this.detailService.getAllDetails().subscribe(response => {
 
-      console.log(this.selectedMuscleGroups)
+        Object.values(res).map((item, index) => {
+          if (item == "true"){
+            this.selectedMuscleGroups.push(response[index]);
+            this.selectedMuscleGroupsView.push({
+              id : response[index].id , className : response[index].className , title : response[index].title , exercises : response[index].exercises.filter((exercise: { detail: { isFavorite: boolean; }; }) => exercise.detail.isFavorite)
+            })
+          }
+        })
+
+      });
 
     });
 
   }
 
-  getAllExercises(name: string) {
-    return this.global.allMuscleGroups.find(i => i.name == name)?.allExercise
+  getFavoriteExercisesLength(muscleGroup : MuscleGroup) : number{
+    return muscleGroup.exercises.filter(item => item.detail.isFavorite == true).length
   }
 
-  setExerciseLengthValue(muscleGroupIndex: number, value: number) {
-    if (value == -1) this.selectedMuscleGroups[muscleGroupIndex].exercises.pop();
-    else this.selectedMuscleGroups[muscleGroupIndex].exercises.push(this.global.getDefaultExercise("" , ""));
+  getAllExercises(muscleGroupName : string){
+    //console.log(this.selectedMuscleGroups.find(item => item.title == muscleGroupName)?.exercises)
+    return this.selectedMuscleGroups.find(item => item.title == muscleGroupName)?.exercises
   }
-  setExerciseNumberOfSets(muscleGroupIndex: number, exerciseIndex: number, value: number) {
-    this.selectedMuscleGroups[muscleGroupIndex].exercises[exerciseIndex].numberOfSets += value
 
-    if (value == -1) this.selectedMuscleGroups[muscleGroupIndex].exercises[exerciseIndex].repWeight.pop();
-    else this.selectedMuscleGroups[muscleGroupIndex].exercises[exerciseIndex].repWeight.push(0);
+  getDefaultExercise() : MuscleGroupExercise{
+    return {
+      detail : {
+        name : "",
+        className : "",
+        isFavorite : false
+      },
+      numberOfSets : 3,
+      numberOfRepetitions : 12,
+      repWeight : [0,0,0],
+      volume : 0
+    }
   }
-  setExerciseNumberOfRepetitions(muscleGroupIndex: number, exerciseIndex: number, value: number) {
-    this.selectedMuscleGroups[muscleGroupIndex].exercises[exerciseIndex].numberOfRepetitions += value
+
+
+  selectedExerciseHandle(muscleGroupName : string , muscleGroupIndex : number , index : number , exerciseTitle : string){
+    const exercise = this.selectedMuscleGroups.find(item => item.title == muscleGroupName)?.exercises.find(item => item.detail.name == exerciseTitle)
+    if(exercise) this.selectedMuscleGroupsView[muscleGroupIndex].exercises.splice(index , 1 , exercise)
+    console.log(this.selectedMuscleGroupsView)
   }
-  setExerciseRepWeight(muscleGroupIndex: number, exerciseIndex: number, setIndex : number, value: number){
-    this.selectedMuscleGroups[muscleGroupIndex].exercises[exerciseIndex].repWeight[setIndex] += value;
+
+
+  setExerciseLengthValue(index : number, value: number) {
+    if (value == -1) this.selectedMuscleGroupsView[index].exercises.pop();
+    else this.selectedMuscleGroupsView[index].exercises.push(this.getDefaultExercise());
+  }
+  setExerciseNumberOfSets(index: number, exerciseIndex: number, value: number) {
+    this.selectedMuscleGroupsView[index].exercises[exerciseIndex].numberOfSets += value
+
+    if (value == -1) this.selectedMuscleGroupsView[index].exercises[exerciseIndex].repWeight.pop();
+    else this.selectedMuscleGroupsView[index].exercises[exerciseIndex].repWeight.push(0);
+  }
+  setExerciseNumberOfRepetitions(index: number, exerciseIndex: number, value: number) {
+    this.selectedMuscleGroupsView[index].exercises[exerciseIndex].numberOfRepetitions += value
+  }
+  setExerciseRepWeight(index: number, exerciseIndex: number, setIndex : number, value: number){
+    this.selectedMuscleGroupsView[index].exercises[exerciseIndex].repWeight[setIndex] += value;
+  }
+
+
+  checkMuscleGroupExercise(muscleGroupName : string) : boolean{
+    return this.selectedMuscleGroups.find(item => item.title == muscleGroupName)?.exercises.length ? true : false
+  }
+  checkExerciseInView(exerciseName : string , index :number){
+    return this.selectedMuscleGroupsView[index].exercises.find(item => item.detail.name == exerciseName) ? true : false
+  }
+
+
+  submitTraining(){
+
+    // this.workoutDayService.addWorkoutDay({
+
+    // })
+
   }
 
 }
